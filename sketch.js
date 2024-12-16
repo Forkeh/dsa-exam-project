627ba8882a1e7b944cf51be696664318b1a965af
Forkeh <forkeh@gmail.com>
2024-12-16 16:47:48 +0100

    WIP with algorithm

--- sketch.js
+++ sketch.js
@@ -1,11 +1,18 @@
-const cols = 5;
-const rows = 5;
+const cols = 25;
+const rows = 25;
 const grid = new Array(cols);
 
-const openSet = [];
+let openSet = [];
 const closedSet = [];
 let start, end;
 let w, h;
+let current;
+let path = [];
+
+function heuristic(a, b) {
+    // Manhatten heuristic
+    return abs(a.x - b.x) + abs(a.y - b.y);
+}
 
 class Spot {
     x;
@@ -13,6 +20,8 @@ class Spot {
     f = 0;
     g = 0;
     h = 0;
+    neighbors = [];
+    previous = null;
     constructor(col, row) {
         this.x = col;
         this.y = row;
@@ -24,6 +33,16 @@ class Spot {
         rect(this.x * w, this.y * h, w - 1, h - 1);
         console.log("SHOW");
     }
+
+    addNeighbors(grid) {
+        if (this.x + 1 < cols) this.neighbors.push(grid[this.x + 1][this.y]);
+
+        if (this.x - 1 >= 0) this.neighbors.push(grid[this.x - 1][this.y]);
+
+        if (this.y + 1 < rows) this.neighbors.push(grid[this.x][this.y + 1]);
+
+        if (this.y - 1 >= 0) this.neighbors.push(grid[this.x][this.y - 1]);
+    }
 }
 
 function setup() {
@@ -34,16 +53,26 @@ function setup() {
     w = width / cols;
     h = height / rows;
 
+    // Create columns and rows
     for (let col = 0; col < cols; col++) {
         grid[col] = new Array(rows);
     }
 
+    // Populate each cell with spot
     for (let col = 0; col < cols; col++) {
         for (let row = 0; row < rows; row++) {
             grid[col][row] = new Spot(col, row);
         }
     }
 
+    // Add neighbors for each spot
+    for (let col = 0; col < cols; col++) {
+        for (let row = 0; row < rows; row++) {
+            grid[col][row].addNeighbors(grid);
+        }
+    }
+
+    // Set start and end cells
     start = grid[0][0];
     end = grid[cols - 1][rows - 1];
 
@@ -63,14 +92,44 @@ function draw() {
                 winner = i;
             }
         }
+        current = openSet[winner];
 
-        if ((openSet[winner] = end)) {
+        if (current === end) {
+            noLoop();
             console.log("Done");
+        }
+
+        // TODO: Does this work?
+        openSet = openSet.filter((spot) => spot !== current);
+
+        closedSet.push(current);
+
+        const neighbors = current.neighbors;
+        for (let i = 0; i < neighbors.length; i++) {
+            const neighbor = neighbors[i];
+
+            if (!closedSet.includes(neighbor)) {
+                const tempG = current.g + 1;
+
+                if (openSet.includes(neighbor)) {
+                    if (tempG < neighbor.g) {
+                        neighbor.g = tempG;
+                    }
+                } else {
+                    neighbor.g = tempG;
+                    openSet.push(neighbor);
+                }
+
+                neighbor.h = heuristic(neighbor, end);
+                neighbor.f = neighbor.g + neighbor.h;
+                neighbor.previous = current;
+            }
         }
     } else {
         // No solution
     }
 
+    // Color every cell
     for (let col = 0; col < cols; col++) {
         for (let row = 0; row < rows; row++) {
             grid[col][row].show(color(255));
@@ -83,5 +142,17 @@ function draw() {
 
     for (let i = 0; i < openSet.length; i++) {
         openSet[i].show(color(0, 255, 0));
+    }
+
+    path = [];
+    let temp = current;
+    path.push(temp);
+    while (temp.previous) {
+        path.push(temp.previous);
+        temp = temp.previous;
+    }
+
+    for (let i = 0; i < path.length; i++) {
+        path[i].show(color(0, 0, 255));
     }
 }
